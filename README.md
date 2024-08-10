b71ce6e832067e06d2a0f3eeef0f9f830ac6cdc2
Vaibhav <52103891+Martian-dev@users.noreply.github.com>
2024-08-10 08:34:07 +0530

    feat: new planets and better structuring of planet component

--- README.md
+++ README.md
@@ -4,7 +4,8 @@
 - [x] remove repeating code from the celetial bodies, and modularize the code.
 - [ ] fix the spin time period issue in the planet component
 - [ ] asteroid belt
-- [ ] outer planets
-- [ ] include a way to control the timestep.
+- [x] outer planets
+- [x] include a way to control the timestep.
 - [ ] slider for timestep
+- [ ] moons rendered conditionally by clicking the planet.
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,118 @@
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import AnimatedStars from "components/animatedstars";
-import Earth from "components/celestial/earth";
 import Sun from "components/celestial/sun";
-// import Planet from "components/celestial/planet";
-import Mercury from "components/celestial/mercury";
-import Venus from "components/celestial/venus";
-import Mars from "components/celestial/mars";
+import Planet from "components/celestial/planet";
+// import Mercury from "components/celestial/mercury";
+// import Venus from "components/celestial/venus";
+// import Earth from "components/celestial/earth";
+// import Mars from "components/celestial/mars";
 import { EffectComposer, Bloom } from "@react-three/postprocessing";
 import CameraPosLog from "helpers/camera-pos";
 // import { Perf } from "r3f-perf";
+
+const planets: {
+  name: string;
+  distance: number;
+  radius: number;
+  spinTimePeriod: number;
+  timerPeriod: number;
+  textureMap: string;
+}[] = [
+  {
+    name: "Mercury",
+    distance: 10,
+    radius: 1,
+    spinTimePeriod: 59,
+    timerPeriod: 88,
+    textureMap: "/assets/mercuryMap.jpg",
+  },
+  {
+    name: "Venus",
+    distance: 20,
+    radius: 1,
+    spinTimePeriod: -243,
+    timerPeriod: -225,
+    textureMap: "/assets/venusMap.jpg",
+  },
+  {
+    name: "Earth",
+    distance: 30,
+    radius: 1,
+    spinTimePeriod: 1,
+    timerPeriod: 365.25,
+    textureMap: "/assets/earthMap.jpg",
+  },
+  {
+    name: "Mars",
+    distance: 40,
+    radius: 1,
+    spinTimePeriod: 1,
+    timerPeriod: 687,
+    textureMap: "/assets/marsMap.jpg",
+  },
+  {
+    name: "Jupiter",
+    distance: 50,
+    radius: 1,
+    spinTimePeriod: 1,
+    timerPeriod: 4330.6,
+    textureMap: "/assets/jupiterMap.jpg",
+  },
+  {
+    name: "Saturn",
+    distance: 60,
+    radius: 1,
+    spinTimePeriod: 1,
+    timerPeriod: 10755.7,
+    textureMap: "/assets/saturnMap.jpg",
+  },
+  {
+    name: "Uranus",
+    distance: 70,
+    radius: 1,
+    spinTimePeriod: 1,
+    timerPeriod: 30687,
+    textureMap: "/assets/uranusMap.jpg",
+  },
+  {
+    name: "Neptune",
+    distance: 80,
+    radius: 1,
+    spinTimePeriod: 1,
+    timerPeriod: 60266.25,
+    textureMap: "/assets/neptuneMap.jpg",
+  },
+];
 
 function App() {
   return (
     <Canvas shadows camera={{ position: [60, 30, 60] }}>
       <color attach={"background"} args={[0, 0, 0]} />
       <EffectComposer>
-        <Bloom intensity={1.3} mipmapBlur luminanceThreshold={1} />
+        <Bloom intensity={1.2} mipmapBlur luminanceThreshold={1} />
       </EffectComposer>
       <OrbitControls />
       <AnimatedStars />
       <ambientLight intensity={0.3} />
       <Sun />
+      {planets.map((planet, key) => (
+        <Planet
+          distance={planet.distance}
+          radius={planet.radius}
+          spinTimePeriod={planet.spinTimePeriod}
+          timeperiod={planet.timerPeriod}
+          timescale={1}
+          textureMap={planet.textureMap}
+          key={key}
+        />
+      ))}
       {/*
-      <Planet
-        distance={20}
-        radius={1}
-        spinTimePeriod={1}
-        timeperiod={365.25}
-        timescale={1}
-        textureMap="/assets/earthMap.jpg"
-      />
-      */}
       <Mercury />
       <Venus />
       <Earth />
       <Mars />
+      */}
       {/* <Perf /> */}
       <CameraPosLog event="mousedown" />
     </Canvas>
